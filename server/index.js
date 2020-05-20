091988dec6180a72382b597110f5d06b00e03a91
David <david.lam296@gmail.com>
2020-05-19 20:29:30 -0400

    Initalize skeleton of project: React & Express.js

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '../dist')));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at port ${process.env.PORT}`);
+  console.log(`Go to: ${process.env.ROOT}:${process.env.PORT}`);
+});
