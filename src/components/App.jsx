f89d2ce4af8fae0466a80db911277be3c3c47ca4
David <david.lam296@gmail.com>
2020-05-20 00:43:18 -0400

    Create Transaction component and render dummy data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Login } from './Login';
 import { Register } from './Register';
 import { Portfolio } from './Portfolio';
 import { Transactions } from './Transactions';
+import { U, T } from '../../database/dummy_data';
 
-export const App = () => (
-  <div>
-    <Portfolio />
-    <Transactions />
-  </div>
-);
+export const App = () => {
+  const [transactions, setTransactions] = useState(T);
+  const [userInfo, setUserInfo] = useState(U);
+
+  return (
+    <div>
+      <Portfolio transactions={transactions} userInfo={userInfo} />
+      <Transactions transactions={transactions} />
+    </div>
+  );
+};
--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+const TYPE_OF_TRANS = ['BUY', 'SELL'];
 
-export const Transactions = (props) => (
-  <div>
-    <h1>Transactions</h1>
-  </div>
-);
+export const Transactions = ({ transactions }) => {
+  return (
+    <div>
+      <h1>Transactions</h1>
+      {transactions.map((t) => (
+        <p key={t.id}>
+          {`${TYPE_OF_TRANS[t.type]} (${t.ticker_symbol}) - ${
+            t.quantity
+          } shares @ $${t.cost}`}
+        </p>
+      ))}
+    </div>
+  );
+};
