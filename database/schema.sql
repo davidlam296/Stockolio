b292ddee58687342034bf14411dd467e0c6502df
David <david.lam296@gmail.com>
2020-05-20 00:06:48 -0400

    Update users table to inclue a balance

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,8 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  email VARCHAR(100) NOT NULL
+  email VARCHAR(100) NOT NULL,
+  balance INTEGER NOT NULL
 );
 
 DROP TABLE IF EXISTS transactions;
