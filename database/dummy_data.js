ad37d0f10847854a231ed2361da05b29cb13e002
David <david.lam296@gmail.com>
2020-05-20 00:41:08 -0400

    Create dummy data

---
+++ database/dummy_data.js
@@ -0,0 +1,41 @@
+export const U = {
+  id: 1,
+  name: 'David',
+  email: 'david@email.com',
+  balance: 5000,
+};
+
+export const T = [
+  {
+    id: 1,
+    type: 0,
+    ticker_symbol: 'AAPL',
+    quantity: 5,
+    cost: 300.15,
+    user_id: 1,
+  },
+  {
+    id: 2,
+    type: 0,
+    ticker_symbol: 'HD',
+    quantity: 10,
+    cost: 200.55,
+    user_id: 1,
+  },
+  {
+    id: 3,
+    type: 0,
+    ticker_symbol: 'F',
+    quantity: 50,
+    cost: 4.78,
+    user_id: 1,
+  },
+  {
+    id: 4,
+    type: 0,
+    ticker_symbol: 'F',
+    quantity: 15,
+    cost: 4.99,
+    user_id: 1,
+  },
+];
