36fd43a94ccc3c26a25c3b78bbf1d66465ba132a
David <david.lam296@gmail.com>
2020-05-26 00:26:47 -0400

    Update nav component for styling, adding logout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import '../styles/navigation.css';
 
-export const Navigation = ({ pages, changePage }) => (
-  <div id="links">
-    {pages.map((pageName) => (
-      <p key={`PAGE+${pageName}`} onClick={() => changePage(pageName)}>
-        {pageName}
-      </p>
-    ))}
-  </div>
-);
+export const Navigation = ({ pages, changePage }) => {
+  return (
+    <div id="links">
+      {pages.map((pageName) => (
+        <p
+          key={`PAGE+${pageName}`}
+          className="link"
+          onClick={() => changePage(pageName)}
+        >
+          {pageName}
+        </p>
+      ))}
+      <p className="link">Logout</p>
+    </div>
+  );
+};
