6ae8591ad5b158675948ae5094d626fc7df5da86
David <david.lam296@gmail.com>
2020-05-31 03:19:25 -0400

    Use https

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes.js');
 
@@ -15,7 +17,17 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening at port ${process.env.PORT}`);
-  console.log(`Go to: ${process.env.ROOT}:${process.env.PORT}`);
-});
+if (process.env.DEV === '1') {
+  app.listen(process.env.PORT, () => {
+    console.log(`Listening at port ${process.env.PORT}`);
+    console.log(`Go to: ${process.env.ROOT}:${process.env.PORT}`);
+  });
+} else {
+  const key = fs.readFileSync(path.join(__dirname, '../selfsigned.key'));
+  const cert = fs.readFileSync(path.join(__dirname, '../selfsigned.crt'));
+  const server = https.createServer({ key, cert }, app);
+  server.listen(process.env.PORT, () => {
+    console.log(`Listening at port ${process.env.PORT}`);
+    console.log(`Go to: ${process.env.ROOT}:${process.env.PORT}`);
+  });
+}
